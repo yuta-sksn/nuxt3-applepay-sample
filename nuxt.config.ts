4988fc25227b05f6a87b51f95a9bb26ed56c538c
Yuta Sukekawa <sukekawa@thymefield.jp>
2024-04-17 21:29:15 +0900

    chore: Settings robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width',
+      htmlAttrs: {
+        lang: 'ja',
+      },
+      meta: [
+        { hid: 'robots', name: 'robots', content: 'noindex' }
+      ],
+    },
+  },
+  modules: [
+    '@nuxtjs/robots'
+  ],
+  robots: {
+    UserAgent: '*',
+    Disallow: '/'
+  }
 })
