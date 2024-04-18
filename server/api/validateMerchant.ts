d44a98e911a4c4fec4dda47d4368d9018f60fe15
Yuta Sukekawa <sukekawa@thymefield.jp>
2024-04-18 22:01:37 +0900

    fix

--- server/api/validateMerchant.ts
+++ server/api/validateMerchant.ts
@@ -39,7 +39,7 @@ export default defineEventHandler(async (event) => {
         merchantIdentifier: body.merchantIdentifier,
         displayName: body.displayName,
         initiative: 'web',
-        domainName: body.domainName,
+        // domainName: body.domainName,
         initiativeContext: body.domainName,
       }));
 
